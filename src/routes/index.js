abbae78ba865dcb38960ae8af6c3f5d8997e1648
Baiguera Paolo <paolo.baiguera@studenti.unitn.it>
2019-11-27 15:05:59 +0100

    Added DS_Store to gitignore.
    Tutti i cambiamenti in src/routes/index.js sono commentati.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 
 const users = require('./users');
+//const reviews = require('./reviews');
 
 module.exports = () => {
   const routes = Router();
   users(routes);
+  //reviews(routes);
 
   return routes;
 };
