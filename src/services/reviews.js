9a4ff8b207f020c68c73ec82c972509e1f346527
Baiguera Paolo <paolo.baiguera@studenti.unitn.it>
2019-11-28 23:49:15 +0100

    Piccole correzioni + aggiornamento package-lock.json

--- src/services/reviews.js
+++ src/services/reviews.js
@@ -1,21 +1,25 @@
-const Review = require('@app/models/reviews');
+const Review = require('@app/models/review');
 
 async function write(review) {
     if (!review) {
         throw Error('Review parameter required.');
     }
 
-    if (!review.review_user_id || !reviewreview_.line_id || !review.review_rating || !review.review_description) {
+    if (!review.review_user_id || !review.review_line_id || !review.review_rating || !review.review_description) {
         throw Error('Please supply a valid Review object: { review_user_id : String, '
             + 'review_line_id : Number, review_rating : Number, review_description: String }');
     }
 
     Review.insert(review);
+    console.log('Added new review');
     return;
 }
 
 async function read(filters) {
     // Change filters
+    /*const res = await Review.getByPrimaryKey(filters.review_id);
+    console.log(res);
+    return res;*/
     return Review.getByPrimaryKey(filters.review_id);
 }
 
