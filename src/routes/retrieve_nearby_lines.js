161c996f7ada35e6c91bb9a9d8ade86bafb47992
Stefano <stefano.facchini@studenti.unitn.it>
2019-12-04 23:34:34 +0100

    Fix data type and name

--- src/routes/retrieve_nearby_lines.js
+++ src/routes/retrieve_nearby_lines.js
@@ -9,6 +9,9 @@ module.exports = async function (routes) {
 
     route.get('/', async (req, res, next) => {
         try {
+
+            console.log(req.query);
+
             const result = await RetrieveService.retrieve(req.query);
             res.status(200).json(result);
         } catch (e) {
