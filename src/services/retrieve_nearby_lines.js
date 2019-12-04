29f4719e277452f24384bf836c9f34213aecb340
Stefano <stefano.facchini@studenti.unitn.it>
2019-12-04 23:27:07 +0100

    Fix retrieving function

--- src/services/retrieve_nearby_lines.js
+++ src/services/retrieve_nearby_lines.js
@@ -15,8 +15,9 @@ async function retrieve(point) {
     let nearby_lines = [];
 
     for (let i = 0; i < existing_lines.length; i++) {
-        if (((Math.abs(existing_lines[i].start_lat - lat) < rad) && (Math.abs(existing_lines[i].start_lon - lon) < rad))
-            || ((Math.abs(existing_lines[i].end_lat - lat) < rad) && (Math.abs(existing_lines[i].end_lon - lon) < rad))) {
+        if (((Math.abs(existing_lines[i].line_start_lat - lat) < rad) && (Math.abs(existing_lines[i].line_start_lon - lon) < rad))
+            || ((Math.abs(existing_lines[i].line_end_lat - lat) < rad) && (Math.abs(existing_lines[i].line_end_lon - lon) < rad))) {
+            console.log("Aggiunta linea con id " + existing_lines[i].line_id)
             nearby_lines.push(existing_lines[i]);
         }
     }
