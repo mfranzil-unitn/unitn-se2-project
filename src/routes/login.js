610a227f35bdcda34ca9f57ee435996b4ea73f5d
simonenascivera <simone.nascivera@gmail.com>
2019-12-09 11:48:06 +0100

    added cookie generation for authorization

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,4 +1,7 @@
+require('dotenv')
+
 const { Router } = require('express');
+const jwt = require('jsonwebtoken')
 
 const UserService = require('@app/services/users');
 
@@ -10,6 +13,17 @@ module.exports = async function (routes) {
     route.post('/', async (req, res, next) => {
         try {
             await UserService.authenticate(req.query);
+            
+            // Create a temporary token for auth 
+            // if auth with db was successful
+            let username = req.query.user_id;
+            if (username) {
+                const token = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET, {
+                    algorithm: 'HS256'
+                    //expiresIn: process.env.EXPIRATION_TOKEN
+                })
+                res.cookie('token', token, { maxAge: process.env.EXPIRATION_TOKEN * 1000 });
+            }
             res.status(200).json();
         } catch (e) {
             const error = new Error('Failed to login: ' + e.message);
