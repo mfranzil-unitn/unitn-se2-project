ea4d06745b6d281c2339561b8721aa096bd8ae44
mfranzil <matteofranzil@gmail.com>
2019-12-13 20:51:43 +0100

    .

--- src/services/users.spec.js
+++ src/services/users.spec.js
@@ -24,23 +24,3 @@ test('invalid authentication should throw an error', async () => {
         user_password: 'nonsense password'
     })).rejects.toThrow('Wrong username or password');
 });
-
-test('find with no parameters should throw an error', async () => {
-    const UserService = require('@app/services/users');
-
-    await expect(UserService.find()).rejects.toThrow('Please supply an array');
-    await expect(UserService.find(undefined)).rejects.toThrow('Please supply an array');
-});
-
-test('find with an empty array should return an empty array', async () => {
-    const UserService = require('@app/services/users');
-
-    let res = await UserService.find([]);
-    expect(Array.isArray(res)).toBe(true);
-});
-
-test('find with a single query should throw an error', async () => {
-    const UserService = require('@app/services/users');
-
-    await expect(UserService.find('non-existant email')).rejects.toThrow('Cannot find User');
-});
